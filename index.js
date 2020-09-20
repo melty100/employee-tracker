1b3ab4a481f8c8e21dcf968bc7bbbe11c671fb99
Connor <melty100@gmail.com>
2020-09-19 18:19:45 -0700

    Added questions for inquirer and logic for logic for sql connection

--- index.js
+++ index.js
@@ -1,5 +1,55 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+var questions = [
+    {
+        type: "list",
+        message: "Would you like to view, add, or update table data?",
+        choices: ["View", "Add", "Update"],
+        name: "action"
+    },
+    {
+        type: "list",
+        message: "Which table are you acting on?",
+        choices: ["employee", "role", "department"],
+        when: (ans) => ans.action == "View" || ans.action == "Add",
+        name: "table"
+    },
+    {
+        type: "input",
+        message: "Enter new department name",
+        when: (ans) => ans.action == 'Add' && ans.table == 'department',
+        name: "department"
+    },
+    {
+        type: "input",
+        message: "Enter employee's first name.",
+        when: (ans) => ans.action == 'Add' && ans.table == "employee",
+        name: "employeeFn"
+    },
+    {
+        type: "input",
+        message: "Enter employee's last name.",
+        when: (ans) => ans.action == 'Add' && ans.table == "employee",
+        name: "employeeLn"
+    },
+    {
+        type: "input",
+        message: "Enter employee's role id",
+        when: (ans) => ans.action == 'Add' && ans.table == "employee",
+        name: "employeeRoleId"
+    },
+    {
+        type: "input",
+        message: "Enter employee's manager id",
+        when: (ans) => ans.action == "Add" && ans.table == "employee",
+        name: "managerId"
+    },
+    {
+        type: "confirm",
+        message: "Are you done querying employee_db?",
+        name: "done"
+    }
+]
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -10,12 +60,38 @@ var connection = mysql.createConnection({
     // Your username
     user: "root",
 
-  // Your password
+    // Your password
     password: "root",
     database: "employee_db"
 });
 
-connection.connect(function(err) {
-    if(err) throw err;
+connection.connect(function (err) {
+    if (err) throw err;
     console.log("connected as id " + connection.threadId);
+
+    let done = false;
+    ans = inquirer.prompt(questions);
+    
+    switch (ans.action) {
+        case "View":
+            viewTable(ans);
+            break;
+        case "Add":
+            insertInto(ans);
+            break;
+        case "View":
+            updateTable(ans);
+            break;
+        default:
+            break;
+    }
+
+    done = ans.done;
+
+
+    connection.end();
 });
+
+function insertInto(ans) { };
+function viewTable(ans) { };
+function updateTable(ans) { };

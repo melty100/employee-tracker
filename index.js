795aaf9033030745088c074e76d363992108eeb6
Connor <melty100@gmail.com>
2020-09-21 13:42:40 -0700

    Added js file for importing quesitons into index.js, and added async functionality for repeated user prompting

--- index.js
+++ index.js
@@ -1,55 +1,6 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
-var questions = [
-    {
-        type: "list",
-        message: "Would you like to view, add, or update table data?",
-        choices: ["View", "Add", "Update"],
-        name: "action"
-    },
-    {
-        type: "list",
-        message: "Which table are you acting on?",
-        choices: ["employee", "role", "department"],
-        when: (ans) => ans.action == "View" || ans.action == "Add",
-        name: "table"
-    },
-    {
-        type: "input",
-        message: "Enter new department name",
-        when: (ans) => ans.action == 'Add' && ans.table == 'department',
-        name: "department"
-    },
-    {
-        type: "input",
-        message: "Enter employee's first name.",
-        when: (ans) => ans.action == 'Add' && ans.table == "employee",
-        name: "employeeFn"
-    },
-    {
-        type: "input",
-        message: "Enter employee's last name.",
-        when: (ans) => ans.action == 'Add' && ans.table == "employee",
-        name: "employeeLn"
-    },
-    {
-        type: "input",
-        message: "Enter employee's role id",
-        when: (ans) => ans.action == 'Add' && ans.table == "employee",
-        name: "employeeRoleId"
-    },
-    {
-        type: "input",
-        message: "Enter employee's manager id",
-        when: (ans) => ans.action == "Add" && ans.table == "employee",
-        name: "managerId"
-    },
-    {
-        type: "confirm",
-        message: "Are you done querying employee_db?",
-        name: "done"
-    }
-]
+var questions = require("./questions.js");
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -69,28 +20,37 @@ connection.connect(function (err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId);
 
-    let done = false;
-    ans = inquirer.prompt(questions);
-    
-    switch (ans.action) {
-        case "View":
-            viewTable(ans);
-            break;
-        case "Add":
-            insertInto(ans);
-            break;
-        case "View":
-            updateTable(ans);
-            break;
-        default:
-            break;
-    }
-
-    done = ans.done;
+    promptUser(questions.list);
 
 
     connection.end();
 });
+
+async function promptUser(quest){
+    let done = false;
+
+    while(!done){
+
+        let ans = await inquirer.prompt(questions.list);
+
+        switch (ans.action) {
+            case "View":
+                viewTable(ans);
+                break;
+            case "Add":
+                insertInto(ans);
+                break;
+            case "View":
+                updateTable(ans);
+                break;
+            default:
+                break;
+        }
+
+        done = ans.done;
+
+    }
+}
 
 function insertInto(ans) { };
 function viewTable(ans) { };
---
+++ questions.js
@@ -0,0 +1,54 @@
+
+var questions = [
+    {
+        type: "list",
+        message: "Would you like to view, add, or update table data?",
+        choices: ["View", "Add", "Update"],
+        name: "action"
+    },
+    {
+        type: "list",
+        message: "Which table are you acting on?",
+        choices: ["employee", "role", "department"],
+        when: (ans) => ans.action == "View" || ans.action == "Add",
+        name: "table"
+    },
+    {
+        type: "input",
+        message: "Enter new department name",
+        when: (ans) => ans.action == 'Add' && ans.table == 'department',
+        name: "department"
+    },
+    {
+        type: "input",
+        message: "Enter employee's first name.",
+        when: (ans) => ans.action == 'Add' && ans.table == "employee",
+        name: "employeeFn"
+    },
+    {
+        type: "input",
+        message: "Enter employee's last name.",
+        when: (ans) => ans.action == 'Add' && ans.table == "employee",
+        name: "employeeLn"
+    },
+    {
+        type: "input",
+        message: "Enter employee's role id",
+        when: (ans) => ans.action == 'Add' && ans.table == "employee",
+        name: "employeeRoleId"
+    },
+    {
+        type: "input",
+        message: "Enter employee's manager id",
+        when: (ans) => ans.action == "Add" && ans.table == "employee",
+        name: "managerId"
+    },
+    {
+        type: "confirm",
+        message: "Are you done querying employee_db?",
+        name: "done"
+    }
+]
+
+exports.list = questions;
+
